f3e4329369821722acc661a732ce1bb48fa3ffe3
pauljiang03 <112199352+pauljiang03@users.noreply.github.com>
2025-10-01 16:55:56 -0400

    Update simple_test.c

--- src/non_monotonic_test.c
+++ src/non_monotonic_test.c
@@ -1,1 +1,14 @@
+#include <assert.h>
+#include <limits.h> 
 
+void main() {
+    int a; 
+    int b;
+    
+
+    __CPROVER_assume(a > 0 && a > (INT_MAX - 10)); 
+    __CPROVER_assume(b > 0 && b < 10);
+    int c = a + b;
+
+    assert(c > a); 
+}
