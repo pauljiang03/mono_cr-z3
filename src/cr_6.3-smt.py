e5459c2ec69ef610e91f8d6a2431c8da2781d70e
pauljiang03 <112199352+pauljiang03@users.noreply.github.com>
2025-10-31 00:20:26 -0400

    Update floating-point precision and constraints

--- src/cr_6.3-smt.py
+++ src/cr_6.3-smt.py
@@ -1,14 +1,10 @@
 from z3 import *
 import time
 
-'''
-Result: unsat (found in 1141.6448 seconds)
-'''
-
 m = 24
 RNE = RNE()
 fp32 = FPSort(8, 24)
-fp64 = FPSort(11, 53)
+fp128 = FPSort(15, 113)
 
 def exp_unbiased_normal(x):
     bv = fpToIEEEBV(x)
@@ -19,11 +15,11 @@ s = Solver()
 
 T0 = FP("T0", fp32)
 T1 = FP("T1", fp32)
-s.add(T1 < T0)
-e0 = exp_unbiased_normal(T0)
-e1 = exp_unbiased_normal(T1) - 24
+s.add(exp_unbiased_normal(T1) <= exp_unbiased_normal(T0))
+max_bit = exp_unbiased_normal(T0)
+min_bit = exp_unbiased_normal(T1) - 24
 
-s.add(Abs(e0 - e1) + 1 <= m)
+s.add(Abs(max_bit - min_bit) + 1 <= m)
 
 print(f"Checking: |e0 - e1| + 1 <= {m}")
 
@@ -31,11 +27,11 @@ s32_0 = FPVal(0.0, fp32)
 s32_1 = fpAdd(RNE, s32_0, T0)
 s32 = fpAdd(RNE, s32_1, T1)
 
-s64_0 = FPVal(0.0, fp64)
-s64_1 = fpAdd(RNE, s64_0, fpToFP(RNE, T0, fp64))
-s64 = fpAdd(RNE, s64_1, fpToFP(RNE, T1, fp64))
+s128_0 = FPVal(0.0, fp128)
+s128_1 = fpAdd(RNE, s128_0, fpToFP(RNE, T0, fp128))
+s128 = fpAdd(RNE, s128_1, fpToFP(RNE, T1, fp128))
 
-s.add(s64 != fpToFP(RNE, s32, fp64))
+s.add(s128 != fpToFP(RNE, s32, fp128))
 
 print("\nSearching for counterexample...")
 start_check_time = time.monotonic()
@@ -50,5 +46,4 @@ if result == sat:
     print(s.model())
 else:
     print("\nunsat")
-
 
