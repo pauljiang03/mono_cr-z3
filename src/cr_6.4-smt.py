01a7841143b471af78456124de3ddfdcc9d351fd
pauljiang03 <112199352+pauljiang03@users.noreply.github.com>
2025-10-31 00:11:31 -0400

    Refactor tensor core verification logic
    Refactor code to improve clarity and efficiency by removing redundant variables and adding new calculations for e_min and e_max.

--- src/cr_6.4-smt.py
+++ src/cr_6.4-smt.py
@@ -1,12 +1,16 @@
 from z3 import *
 import time
 
-N = 1024
+'''
+N can be set 5, 9, or 16 to verify for any existing tensor core architecture
+For N <= 128, can prove in under 1 minute
+'''
+
+N = 5
 m = 24
 
 RNE = RNE()
 fp32 = FPSort(8, 24)
-
 
 def ceil_log2(n):
     k, v = 0, 1
@@ -17,7 +21,6 @@ def ceil_log2(n):
         k += 1
     return k
 
-
 def exp_unbiased_normal(x):
     bv = fpToIEEEBV(x)
     exp = Extract(30, 23, bv)
@@ -26,45 +29,61 @@ def exp_unbiased_normal(x):
 
 s = Solver()
 
+T = [FP(f"T{i}", fp32) for i in range(N)]
+e_list = []
 
-T_orig = [FP(f"T_orig{i}", fp32) for i in range(N)]
-Eps = [FP(f"Eps{i}", fp32) for i in range(N)]
-
-e_T_list = []
-e_eps_list = []
-
-for i in range(N):
-    s.add(And(Not(fpIsNaN(T_orig[i])), Not(fpIsInf(T_orig[i]))))
-    s.add(And(Not(fpIsNaN(Eps[i])), Not(fpIsInf(Eps[i]))))
-    e_T_list.append(exp_unbiased_normal(T_orig[i]))
-    e_eps_list.append(exp_unbiased_normal(Eps[i]))
-
-growth = ceil_log2(N)
+for i, ti in enumerate(T):
+    e_list.append(exp_unbiased_normal(ti))
+    not_exceptional = And(Not(fpIsNaN(ti)), Not(fpIsInf(ti)))
+    s.add(not_exceptional)
 
 
-e_T_min = e_T_list[0]
-e_T_max = e_T_list[0]
+e_min = e_list[0]
+e_max = e_list[0]
 
 for i in range(1, N):
-    e_current = e_T_list[i]
-    e_T_min = If(e_current < e_T_min, e_current, e_T_min)
-    e_T_max = If(e_current > e_T_max, e_current, e_T_max)
+    e_current = e_list[i]
+    e_min = If(e_current < e_min, e_current, e_min)
+    e_max = If(e_current > e_max, e_current, e_max)
 
-    K_span_6_5 = (e_T_max - e_T_min) + 2 * growth
-    s.add(K_span_6_5 <= m)
+growth = ceil_log2(N)
+K_span = e_max - (e_min - 24) + growth
 
-e_eps_max_set_bit = e_T_max - m
-e_eps_min_set_bit = e_T_min - m
+print(f"(e_max - e_min) + ceil_log2({N}) <= {m}")
+s.add(K_span <= m)
 
-thm_6_4 = (e_eps_max_set_bit - e_eps_min_set_bit) + growth < m
-s.add(Not(thm_6_4))
+step_premises = []
+
+
+s32_intermediate_sum = T[0]
+for i in range(1, N):
+    t_a = s32_intermediate_sum
+    t_b = T[i]
+
+    e_a = exp_unbiased_normal(t_a)
+    e_b = e_list[i]
+    e_max_2 = If(e_a > e_b, e_a, e_b)
+    e_min_2 = If(e_a < e_b, e_a, e_b)
+
+    one_span = e_max_2 - (e_min_2 - 24) + 1
+
+    step_premise_holds = (one_span <= m)
+    step_premises.append(step_premise_holds)
+
+    s32_intermediate_sum = fpAdd(RNE, s32_intermediate_sum, t_b)
+
+
+
+all_steps_hold = And(step_premises)
+all_steps_not_hold = Not(all_steps_hold)
+s.add(all_steps_not_hold)
 
 print("Running solver...")
 start_check_time = time.monotonic()
 result = s.check()
 end_check_time = time.monotonic()
 check_duration = end_check_time - start_check_time
-print(f"\nResult: {result} (found in {check_duration:.4f} seconds)")
+print(f"Result: {result} (found in {check_duration:.4f} seconds)")
 
 if result == sat:
     print("\nsat")
@@ -72,7 +91,5 @@ if result == sat:
     print(model)
 else:
     print("\nunsat")
-
-
 
 
