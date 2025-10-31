116d7ccb2d6f1095238065a9bb407f48cd6f6c49
pauljiang03 <112199352+pauljiang03@users.noreply.github.com>
2025-10-31 00:09:54 -0400

    Update cr_6.3-smt.py

--- src/cr_6.3-smt.py
+++ src/cr_6.3-smt.py
@@ -7,8 +7,8 @@ Result: unsat (found in 1141.6448 seconds)
 
 m = 24
 RNE = RNE()
-S32 = FPSort(8, 24)
-S64 = FPSort(11, 53)
+fp32 = FPSort(8, 24)
+fp64 = FPSort(11, 53)
 
 def exp_unbiased_normal(x):
     bv = fpToIEEEBV(x)
@@ -17,8 +17,8 @@ def exp_unbiased_normal(x):
 
 s = Solver()
 
-T0 = FP("T0", S32)
-T1 = FP("T1", S32)
+T0 = FP("T0", fp32)
+T1 = FP("T1", fp32)
 s.add(T1 < T0)
 e0 = exp_unbiased_normal(T0)
 e1 = exp_unbiased_normal(T1) - 24
@@ -27,15 +27,15 @@ s.add(Abs(e0 - e1) + 1 <= m)
 
 print(f"Checking: |e0 - e1| + 1 <= {m}")
 
-s32_0 = FPVal(0.0, S32)
+s32_0 = FPVal(0.0, fp32)
 s32_1 = fpAdd(RNE, s32_0, T0)
 s32 = fpAdd(RNE, s32_1, T1)
 
-s64_0 = FPVal(0.0, S64)
-s64_1 = fpAdd(RNE, s64_0, fpToFP(RNE, T0, S64))
-s64 = fpAdd(RNE, s64_1, fpToFP(RNE, T1, S64))
+s64_0 = FPVal(0.0, fp64)
+s64_1 = fpAdd(RNE, s64_0, fpToFP(RNE, T0, fp64))
+s64 = fpAdd(RNE, s64_1, fpToFP(RNE, T1, fp64))
 
-s.add(fpToFP(RNE, s64, S32) != s32)
+s.add(s64 != fpToFP(RNE, s32, fp64))
 
 print("\nSearching for counterexample...")
 start_check_time = time.monotonic()
@@ -50,4 +50,5 @@ if result == sat:
     print(s.model())
 else:
     print("\nunsat")
+
 
